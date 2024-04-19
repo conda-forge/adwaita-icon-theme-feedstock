e2c28ecf5454997eb00333636828a719fe025609
Ryan Volz <ryan.volz@gmail.com>
2024-04-19 16:59:10 -0400

    Catch negative errorlevels on Windows, use builtin gtk-update-icon-cache

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,15 +1,11 @@
 @echo on
 
 meson setup builddir --buildtype=release --prefix="%LIBRARY_PREFIX%"
-if errorlevel 1 exit 1
+if %ERRORLEVEL% neq 0 exit 1
 
 :: print results of build configuration
 meson configure builddir
-if errorlevel 1 exit 1
+if %ERRORLEVEL% neq 0 exit 1
 
 meson install -C builddir
-if errorlevel 1 exit 1
-
-:: build icon cache manually since it fails for some reason when run by meson
-"%BUILD_PREFIX%\Library\bin\gtk-update-icon-cache.exe" -f -t "%LIBRARY_PREFIX%\share\icons\Adwaita"
-if errorlevel 1 exit 1
+if %ERRORLEVEL% neq 0 exit 1
